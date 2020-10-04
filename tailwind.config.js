0a70bf966151c7a4613695fbfeb0f1af03c2efd9
Bernard Zhao <bernard.zhao.us@gmail.com>
2020-10-04 16:43:10 -0700

    fix tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'src/components/**/*.vue',
+      'src/layouts/**/*.vue',
+      'src/pages/**/*.vue',
+      'src/templates/**/*.vue',
+    ]
+  },
   theme: {
     extend: {
       animation: {
